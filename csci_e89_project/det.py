503a2bf2a1e7ea52a61d39b6dd253176ea371758
JaeungHyun <jaeung@me.com>
2019-02-17 18:06:54 +0900

    fix

--- csci_e89_project/det.py
+++ csci_e89_project/det.py
@@ -370,10 +370,10 @@ class FishDataset(utils.Dataset):
     
         class_ids = []
         if image_info['id'] in self.result_masks.keys():
-            class_name = get_class_name(image_info['id'])
+            class_name = self.get_class_name(image_info['id'])
             for class_info in self.class_info:
-                if class_name == self.class_info['name']:
-                    class_ids.append(self.class_info['id'])
+                if class_name == class_info['name']:
+                    class_ids.append(class_info['id'])
         
         # Return mask, and array of class IDs of each instance.
         # 마스크를 돌려줄 때, 각 인스턴스의 클래스 아이디 번호를 넘파이 배열로 넘겨줘야한다.
